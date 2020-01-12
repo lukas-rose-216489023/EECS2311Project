aadbeb6d5e5dd731e3833bae77a096a005e76236
sarwat12 <sarwat.shaheen@yahoo.com>
2020-01-11 21:35:27 -0500

    Added new print statement.

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		System.out.println("Hello World!");
 		System.out.println("How are you?");
 		System.out.println("Test1?");
-		System.out.println("Test2!");
+		System.out.println("New test!");
 	}
 
 }
