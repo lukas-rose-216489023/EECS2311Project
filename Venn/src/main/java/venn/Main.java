69eb83d27b26102d03272067b67fb3dd0881c67a
Lukas Rose <lukayork@my.yorku.ca>
2020-01-13 12:20:36 -0500

    new push

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		System.out.println("Hello World!");
 		System.out.println("How are you?");
 		System.out.println("New test!");
-		//Added a new comment!
+		//Added a new comment
 	}
 
 }
