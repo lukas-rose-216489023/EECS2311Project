419f17481eea593c2d437f00128c4a6d41d24aab
Lukas Rose <lukayork@my.yorku.ca>
2020-01-13 22:45:02 -0500

    Made Changes to my class

--- Venn/src/main/java/venn/FirstGUI.java
+++ Venn/src/main/java/venn/FirstGUI.java
@@ -1,12 +1,44 @@
 package venn;
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
-public class FirstGUI extends JFrame {
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.*;
+
+public class FirstGUI {
+	
+	private JFrame f;
+	private JPanel p;
+	private JButton b1;
+	private JLabel lab;
+	
+	public FirstGUI() {
+		
+		gui();
+	}
+	
+	public void gui() {
+		
+		f = new JFrame("First GUI");
+		f.setVisible(true);
+		f.setSize(600, 400);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		p = new JPanel();
+		p.setBackground(Color.yellow);
+		
+		b1 = new JButton("O");
+		lab = new JLabel("Create a Circle");
+		
+		p.add(b1);
+		p.add(lab);
+		
+		f.add(p);
+		f.add(p,BorderLayout.SOUTH);
+	}
+	
 	public static void main (String args[]) {
-		FirstGUI gui = new FirstGUI();
-		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setSize(1000, 1000);
-		gui.setVisible(true);
-		gui.setTitle("First GUI");
+		FirstGUI a = new FirstGUI();
 	}
 }
