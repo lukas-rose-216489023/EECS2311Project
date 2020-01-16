8c2332b6fd01b63a4629848ebd28cb6051b92bdd
Jawad Ahmed <ja64@my.yorku.ca>
2020-01-15 21:13:25 -0500

    Added features and optimization:
    Removed unneeded lines.
    The intersection is now visible - opacity lowered.

--- Venn/src/main/java/venn/VennBase.java
+++ Venn/src/main/java/venn/VennBase.java
@@ -12,31 +12,34 @@ public class VennBase extends Application	 {
 	@Override 
 	public void start(Stage primaryStage) {
 		
+		Stage stage = new Stage();
+		
+		Color blue = new Color(Color.BLUE.getRed(), Color.BLUE.getGreen(), Color.BLUE.getBlue(), 0.75);		
+		Color red = new Color(Color.RED.getRed(), Color.RED.getGreen(), Color.RED.getBlue(), 0.75);
+		
 		Pane pane = new Pane();
-		Stage stage2 = new Stage();
 		Circle circle = new Circle();
 		circle.centerYProperty().bind(pane.heightProperty().divide(2.0));	
 		circle.centerXProperty().bind(pane.widthProperty().divide(5.0/3.0));
 		circle.radiusProperty().bind(pane.widthProperty().divide(5.0));
-//		circle.setRadius(100);
 		circle.setStroke(Color.BLUE);
-		circle.setFill(Color.RED);
-
+		circle.setFill(red);
+		
+		Pane pane2 = new Pane();
 		Circle circle2 = new Circle();
 		circle2.centerYProperty().bind(pane.heightProperty().divide(2.0));												
 		circle2.centerXProperty().bind(pane.widthProperty().divide(5.0/2.0));
 		circle2.radiusProperty().bind(pane.widthProperty().divide(5.0));
-//		circle2.setRadius(100);
 		circle2.setStroke(Color.RED);
-		circle2.setFill(Color.BLUE);
+		circle2.setFill(blue);
 		
 		pane.getChildren().add(circle);
 		pane.getChildren().add(circle2);
 		
 		Scene scene2 = new Scene(pane, 600, 500);
-		stage2.setTitle("Venn Demo");
-		stage2.setScene(scene2);
-		stage2.show();
+		stage.setTitle("Venn Demo");
+		stage.setScene(scene2);
+		stage.show();
 		
 		
 	}
