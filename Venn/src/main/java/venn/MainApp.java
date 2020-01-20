fd1cb51e0762cc6e2e8591d5d9377103d8da61d7
Lukas Rose <lukayork@my.yorku.ca>
2020-01-20 12:14:32 -0500

    changes

--- Venn/src/main/java/venn/MainApp.java
+++ Venn/src/main/java/venn/MainApp.java
@@ -1,56 +1,45 @@
 package venn;
-import java.awt.Color;
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
-
-import javax.swing.JFrame;
 
 import javafx.application.Application;
 
-import javax.swing.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuButton;
+import javafx.scene.layout.StackPane;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import javafx.stage.Stage;
+
  
  
-public class MainApp extends JFrame{
-     
-    static JFrame window = new JFrame();//STATIC: We can only have one window.
-    static JMenuBar menuBar = new JMenuBar();
-    static JMenu menu, submenu;
-    static JMenuItem menuItem;
+public class MainApp extends Application{
     
-     
-    public static void main(String[] args) {
-         
-        createWindow();
-        createMenu();
-        
-         
-         
-         
-    }
-     
-    public static void createWindow()
+	Menu mainmenu;
+	Button button1;   
+    
+	
+    public static void main(String[] args) 
     {
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Toolkit tk = Toolkit.getDefaultToolkit();
-        int xSize = ((int) tk.getScreenSize().getWidth());
-        int ySize = ((int) tk.getScreenSize().getHeight());
-        window.setSize(xSize,ySize);
-        window.setVisible(true); 
+        launch(args);
+             
     }
-    	
-    public static void createMenu() 
+     
+    public void start(Stage primaryStage) throws Exception
     {
-    	menu = new JMenu("Menu");    	
-    	menu.setMnemonic(KeyEvent.VK_A);
-    	menu.getAccessibleContext().setAccessibleDescription("Menu for Venn Diagram Maker");
-    	
-    	menuItem = new JMenuItem("Venn Diagram");
-    	menuItem.addActionListener((event) -> new VennBase());;
-    	
-    	menuBar.add(menu);
-    	window.setJMenuBar(menuBar);
-    	menu.add(menuItem);
+        primaryStage.getScene(menuScene);
     }
+    
+    public void menu(Scene menuScene) throws Exception
+    {
+        menuScene.
+    }
+    
+    	
+  
     
     
     
