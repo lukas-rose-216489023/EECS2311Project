a82b3685dd89f5df675278b38ae39e1211c49e82
marcoywm <marcoywm@hotmail.com>
2020-01-20 12:24:06 -0500

    TestBranch2

--- Venn/src/main/java/venn/BranchTest.java
+++ Venn/src/main/java/venn/BranchTest.java
@@ -5,6 +5,8 @@ public class BranchTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test Branch");
+		
+		System.out.println("test Branch2");
 	}
 
 }
