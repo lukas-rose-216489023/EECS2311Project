509860b374fd6df9ed90ebdc0b3d1c0f227cdc99
Jawad Ahmed <ja64@my.yorku.ca>
2020-01-22 13:14:23 -0500

    Tried different implementations of the 'object'

--- Venn/src/main/java/venn/VennBase.java
+++ Venn/src/main/java/venn/VennBase.java
@@ -2,12 +2,15 @@ package venn;
 
 import javafx.application.Application;
 import javafx.beans.binding.DoubleBinding;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class VennBase extends Application	 {
 	
@@ -18,8 +21,9 @@ public class VennBase extends Application	 {
 		Stage stage = new Stage();
 		
 		//Custom colors
-		Color blue = new Color(Color.BLUE.getRed(), Color.BLUE.getGreen(), Color.BLUE.getBlue(), 0.75);		
-		Color red = new Color(Color.RED.getRed(), Color.RED.getGreen(), Color.RED.getBlue(), 0.75);
+		Color blue = new Color(Color.BLUE.getRed(), Color.BLUE.getGreen(), Color.BLUE.getBlue(), 0.25);		
+		Color red = new Color(Color.RED.getRed(), Color.RED.getGreen(), Color.RED.getBlue(), 0.25);
+		Color green = new Color(Color.GREEN.getRed(), Color.GREEN.getGreen(), Color.GREEN.getBlue(), 0.5);
 		
 		//First venn circle
 		Pane pane = new Pane();
@@ -38,12 +42,23 @@ public class VennBase extends Application	 {
 		circle2.setStroke(Color.RED);
 		circle2.setFill(blue);
 		
-		//Text box within first circle
+		//Text box within first circle - 1st implementation
+//		int sGreen = green();
 		Button box1 = new Button("Insert text here");
-		box1.layoutXProperty().bind(pane.widthProperty().divide(5.0/2.0));
+		box1.layoutXProperty().bind(pane.widthProperty().divide(30.0/13.0));
 		box1.layoutYProperty().bind(pane.heightProperty().divide(2.0));
-		box1.prefWidthProperty().bind(pane.widthProperty().divide(10.0));
+		box1.prefWidthProperty().bind(pane.widthProperty().divide(8.0));
 		box1.prefHeightProperty().bind(pane.heightProperty().divide(10.0));
+//		box1.setStyle("-fx-background-color: #"+sGreen);
+		box1.setOnMouseClicked(changeButtonText(box1, "Insert text here"));
+
+		//Text box within first circle - 2nd implementation
+//		Rectangle box1 = new Rectangle(100, 100);
+//		box1.xProperty().bind(pane.widthProperty().divide(30.0/13.0));
+//		box1.yProperty().bind(pane.heightProperty().divide(2.0));
+//		box1.widthProperty().bind(pane.widthProperty().divide(8.0));
+//		box1.heightProperty().bind(pane.heightProperty().divide(10.0));
+//		box1.setFill(green);
 		
 		//Adds items to the window
 		pane.getChildren().add(circle);
@@ -58,7 +73,10 @@ public class VennBase extends Application	 {
 		
 	}
 	
-	
+	public EventHandler<? super MouseEvent> changeButtonText(Button b, String s) {
+		b.setText(s);
+		return null;
+	}
 	
 	
 	public static void main(String[] args) {
