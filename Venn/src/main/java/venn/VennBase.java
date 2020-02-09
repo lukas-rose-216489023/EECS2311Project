34ee3aa1ba897d2d4f26ea8df397dc720d4f905c
Jawad Ahmed <ja64@my.yorku.ca>
2020-02-08 23:08:20 -0500

    Changes:
    Removed unneeded classes
    Centered Titles
    Improved text box stacking
    Text boxes can now be deleted by right clicking and entering no text

--- Venn/src/main/java/venn/VennBase.java
+++ Venn/src/main/java/venn/VennBase.java
@@ -14,13 +14,14 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextInputDialog;
 									
+@SuppressWarnings("unused")
 public class VennBase extends Application	 {
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -36,11 +37,10 @@ public class VennBase extends Application	 {
 	    Rectangle2D screenBounds = Screen.getPrimary().getBounds();
 		
 		//Custom colors
-		Color blue = new Color(Color.BLUE.getRed(), Color.BLUE.getGreen(), Color.BLUE.getBlue(), 0.25);		
-		Color red = new Color(Color.RED.getRed(), Color.RED.getGreen(), Color.RED.getBlue(), 0.25);
+		Color blue = new Color(Color.BLUE.getRed(), Color.BLUE.getGreen(), Color.BLUE.getBlue(), 0.5);		
+		Color red = new Color(Color.RED.getRed(), Color.RED.getGreen(), Color.RED.getBlue(), 0.5);
 		@SuppressWarnings("unused")
 		Color green = new Color(Color.GREEN.getRed(), Color.GREEN.getGreen(), Color.GREEN.getBlue(), 0.5);
-
 	    
 		//Right venn circle
 		Circle circleR = new Circle();
@@ -48,7 +48,7 @@ public class VennBase extends Application	 {
 		circleR.centerXProperty().bind(pane.widthProperty().divide(5.0/3.0));
 		circleR.radiusProperty().bind(pane.widthProperty().divide(5.0));
 		circleR.setStroke(Color.BLUE);
-		circleR.setFill(red);
+		circleR.setFill(blue);
 		
 		//Left venn circle
 		Circle circleL= new Circle();
@@ -56,16 +56,16 @@ public class VennBase extends Application	 {
 		circleL.centerXProperty().bind(pane.widthProperty().divide(5.0/2.0));
 		circleL.radiusProperty().bind(pane.widthProperty().divide(5.0));
 		circleL.setStroke(Color.RED);
-		circleL.setFill(blue);
+		circleL.setFill(red);
 		
 		
 		//color picker setup
-		final ColorPicker cp1 = new ColorPicker(red);
-		final ColorPicker cp2 = new ColorPicker(blue);
+		final ColorPicker cp1 = new ColorPicker(Color.RED);
+		final ColorPicker cp2 = new ColorPicker(Color.BLUE);
+		final ColorPicker cp3 = new ColorPicker(Color.GREEN);
 		cp1.layoutYProperty().bind(pane.heightProperty().subtract(25));
 		cp2.layoutYProperty().bind(pane.heightProperty().subtract(50));
-//		cp1("Right circle color");								need to change title
-//		cp2.setAccessibleText("Left circle color");				need to change title
+		cp3.layoutYProperty().bind(pane.heightProperty().subtract(75));
 		
 		cp1.setOnAction(new EventHandler() {
 			@Override
@@ -91,6 +91,15 @@ public class VennBase extends Application	 {
 				}
         });
 		
+		cp3.setOnAction(new EventHandler() {
+			@Override
+			public void handle(javafx.event.Event event) {
+				String textBo = Integer.toHexString(cp3.getValue().hashCode());//String.format("#%02X%02X%02X", ((int)cp3.getValue().getRed())*255, ((int)cp3.getValue().getGreen())*255, ((int)cp3.getValue().getBlue())*255);
+//				textBo = "-fx-background-color: #" + textBo;
+//				Record.textBox = textBo;
+				}
+        });
+		
 		
 		//text box adder
 		Button textAdder = new Button("Add new text box");
@@ -101,27 +110,29 @@ public class VennBase extends Application	 {
 		textAdder.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {		
-				
+				int stackable = Math.floorDiv((int) screenBounds.getHeight(), (int) textAdder.getHeight())-2;
 				//Text box prototype - 1st implementation
-				Button box = new Button("Insert text here 123456789");
-				Record.numBoxes++;
-				boolean moved = false;
-				box.prefWidthProperty().bind(pane.widthProperty().divide(8.0));
+				Button box = new Button("New Text Box");
+//				boolean moved = false;
+				box.prefWidthProperty().bind(pane.widthProperty().divide(7.0));
 				box.prefHeightProperty().bind(pane.heightProperty().divide(15.0));
 //				box.setLayoutX(screenBounds.getWidth()-(screenBounds.getWidth()/8));
 				box.setLayoutX(textAdder.getLayoutX());
-				box.setLayoutY(textAdder.getLayoutY()-(textAdder.getHeight()*(Record.numBoxes%12)));
-
+				box.setLayoutY(textAdder.getLayoutY()-(textAdder.getHeight()*(Record.numBoxes%stackable)+textAdder.getHeight()));
+				Record.numBoxes++;
+				
+//				box.setStyle("-fx-background-color: "+Record.textBox);
 				box.setStyle("-fx-background-color: #80b380");
 
 				//Change contents
 				box.setOnMouseClicked(new EventHandler<MouseEvent>() {
 					@Override
 					public void handle(MouseEvent event) {
+//						box.setText(Record.textBox);
 						if (event.getButton() == MouseButton.SECONDARY) {
 							TextInputDialog dialog = new TextInputDialog(box.getText());
 							dialog.setTitle("Change text");
-							dialog.setHeaderText("Enter to change text");
+							dialog.setHeaderText("Enter to change text\nLeave empty to delete text box");
 							dialog.setContentText("25 character limit");
 							String result = dialog.showAndWait().get();
 							while (result.length()>25) {
@@ -129,6 +140,7 @@ public class VennBase extends Application	 {
 								result = dialog.showAndWait().get();
 								}
 							box.setText(result);
+							if (box.getText().equals("")) {pane.getChildren().remove(box);}
 						}
 					}
 				});
@@ -166,7 +178,7 @@ public class VennBase extends Application	 {
 		Text title = new Text("Title");
 		title.setFont(new Font(20));
 		title.setStroke(Color.BLACK);
-		title.layoutXProperty().bind(pane.widthProperty().divide(2.0));
+		title.setLayoutX(((int) screenBounds.getWidth()/2)-title.getText().length()*8/2);			//does not support window resizing
 		title.setLayoutY(25);
 		title.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
@@ -182,6 +194,7 @@ public class VennBase extends Application	 {
 						result = dialog.showAndWait().get();
 						}
 					title.setText(result);
+					title.setLayoutX(((int) screenBounds.getWidth()/2)-title.getText().length()*8/2);
 				}
 			}
 		});
@@ -190,7 +203,8 @@ public class VennBase extends Application	 {
 		left.setFont(new Font(12));
 		left.setStroke(Color.BLACK);
 		left.setTextAlignment(TextAlignment.CENTER);
-		left.layoutXProperty().bind(pane.widthProperty().divide(5.0/2.0).subtract(25));
+//		left.layoutXProperty().bind(pane.widthProperty().divide(5.0/2.0).subtract(25));
+		left.setLayoutX(((int) screenBounds.getWidth()/2.5)-left.getText().length()*5/2);
 		left.layoutYProperty().bind(pane.heightProperty().divide(2.0).subtract(circleL.radiusProperty()).subtract(5));
 		left.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
@@ -206,6 +220,7 @@ public class VennBase extends Application	 {
 						result = dialog.showAndWait().get();
 						}
 					left.setText(result);
+					left.setLayoutX(((int) screenBounds.getWidth()/2.5)-left.getText().length()*5/2);
 				}
 			}
 		});
@@ -214,7 +229,8 @@ public class VennBase extends Application	 {
 		right.setFont(new Font(12));
 		right.setStroke(Color.BLACK);
 		right.setTextAlignment(TextAlignment.CENTER);
-		right.layoutXProperty().bind(pane.widthProperty().divide(5.0/3.0).subtract(25));
+//		right.layoutXProperty().bind(pane.widthProperty().divide(5.0/3.0).subtract(25));
+		right.setLayoutX(((int) screenBounds.getWidth()/(5.0/3.0))-right.getText().length()*5/2);
 		right.layoutYProperty().bind(pane.heightProperty().divide(2.0).subtract(circleL.radiusProperty()).subtract(5));
 		right.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
@@ -230,6 +246,7 @@ public class VennBase extends Application	 {
 						result = dialog.showAndWait().get();
 						}
 					right.setText(result);
+					right.setLayoutX(((int) screenBounds.getWidth()/(5.0/3.0))-right.getText().length()*5/2);
 				}
 			}
 		});
@@ -246,13 +263,15 @@ public class VennBase extends Application	 {
 		pane.getChildren().add(left);
 		pane.getChildren().add(right);
 		
+		//Center line - use when you want to center nodes
+//		Line center = new Line(screenBounds.getWidth()/(5.0/3.0), 0, screenBounds.getWidth()/(5.0/3.0), 1000);
+//		pane.getChildren().add(center);
 		
 		root.getChildren().addAll(pane);
 		Scene scene2 = new Scene(root);
 		stage.setTitle("Venn Application Demo");
 		stage.setScene(scene2);
 		stage.show();
-		
 		
 	}
 	
@@ -265,6 +284,8 @@ public class VennBase extends Application	 {
 	//Class for recording
 	static class Record {
 		static int numBoxes;
+		//Default text box color stored in this circle
+//		static String textBox = null;
 	}
 	
 	public static void main(String[] args) {
